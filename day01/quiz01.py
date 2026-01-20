c8aaaddbbf89fb488c4418c220f8e31dc461e76e
정대석 <jds1205jds@gmail.com>
2026-01-21 00:12:02 +0900

    day01 if문 코드

---
+++ day01/quiz01.py
@@ -0,0 +1,12 @@
+for i in range(1,21):
+    if i%2==0 :
+        print(i, "짝수")
+    else :
+        print(i, "홀수")
+###############################
+age = input("나이입력 : ")
+age = int(age)
+if age >= 20 :
+    print("성인")
+else :
+    print("미성년자")
