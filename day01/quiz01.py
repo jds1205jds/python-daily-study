e4743fcc5d15ae0ce212b29b66a80b6fd68ed5da
정대석 <jds1205jds@gmail.com>
2026-01-21 21:46:12 +0900

    gpt가 이론정리를 바탕으로 출제해준 미니퀴즈

--- day01/quiz01.py
+++ day01/quiz01.py
@@ -1,9 +1,13 @@
+
+#1번
 for i in range(1,21):
     if i%2==0 :
         print(i, "짝수")
     else :
         print(i, "홀수")
-###############################
+
+
+#2번
 age = input("나이입력 : ")
 age = int(age)
 if age >= 20 :
