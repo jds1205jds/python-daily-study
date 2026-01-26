4acc786a535085a5217c8bc8c4024ac063afc483
정대석 <jds1205jds@gmail.com>
2026-01-26 23:42:36 +0900

    딕셔너리

---
+++ day02/02.py
@@ -0,0 +1,12 @@
+dictionary = {
+    'name' : '7D 건조 망고',
+    'type' : '당절임',
+    'ingredient' : ['망고', '설탕', '메타중아황산나크륨', '치자황색소'],
+    'origin' : '필리핀'
+}
+
+print('제품이름 : ', dictionary['name'])
+print('종류 : ', dictionary['type'])
+
+dictionary['name'] = '8D 건조 망고'
+print('제품이름 : ', dictionary['name'])
