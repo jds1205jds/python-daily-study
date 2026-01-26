c850b4c3fcd00f23a529814a288d3b875e9f4fb0
정대석 <jds1205jds@gmail.com>
2026-01-26 23:50:11 +0900

    int(input())활용 넓이 구하기

---
+++ day01/01.py
@@ -0,0 +1,10 @@
+# 두 정수 width, height를 입력받아 사각형의 넓이를 구하는
+# 프로그램을 작성해보세요.
+
+width = int(input('width를 입력하세요 : '))
+height = int(input('height를 입력하세요 : '))
+
+# 넓이를 출력하세요.
+area = width * height
+
+print('넓이 : ', area)
