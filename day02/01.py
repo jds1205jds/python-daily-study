7c87d6cfb85b5fa412bd0f33df6b10748018836a
정대석 <jds1205jds@gmail.com>
2026-01-26 23:40:58 +0900

    for문 예제

---
+++ day02/01.py
@@ -0,0 +1,23 @@
+for i in range(100):
+    print(i, end=' ')
+
+print()
+print()
+
+list = [1, 2, 3, 4, 5]
+for i in list:
+    print(i, end=' ')
+
+print()
+print()
+
+for j in range(1, 101, 1):   #100을 출력하고 싶다면
+    print(j, end=' ')
+
+print()
+print()
+
+for j in range(100, -1, -1):     #0까지 출력하고 싶다면
+    print(j, end=' ')
+
+print()
